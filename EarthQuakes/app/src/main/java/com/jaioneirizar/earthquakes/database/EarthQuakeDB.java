d76b8075e88b43d17cf89cdb3d3b1b49cf8bfd22
jaioneirizar <jaioner14@gmail.com>
2015-03-30 19:27:18 +0200

    EarthQuakes DB

--- EarthQuakes/app/src/main/java/com/jaioneirizar/earthquakes/database/EarthQuakeDB.java
+++ EarthQuakes/app/src/main/java/com/jaioneirizar/earthquakes/database/EarthQuakeDB.java
@@ -1,24 +1,177 @@
 package com.jaioneirizar.earthquakes.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.jaioneirizar.earthquakes.model.EarthQuake;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by cursomovil on 27/03/15.
  */
 public class EarthQuakeDB {
 
-    private final SQLiteDatabase db;
-    private EarthQuakeOpenHelper helper;
+    private  SQLiteDatabase db;
+    private EarthQuakeOpenHelper dbhelper;
+    private EarthQuakeDB Helper;
+
+    //columnas de la tabla//
+
+    public static final String KEY_ID = "_id";
+    public static final String KEY_PLACE = "place";
+    public static final String KEY_MAGNITUDE = "magnitude";
+    public static final String KEY_LAT = "lat";
+    public static final String KEY_LONG = "Long";
+    public static final String KEY_URL = "url";
+    public static final String KEY_DEPTH = "depth";
+    public static final String KEY_TIME = "time";
+
+    public static final String[] KEYS_ALL = {KEY_ID,KEY_PLACE,KEY_MAGNITUDE, KEY_LAT, KEY_LONG, KEY_URL,KEY_DEPTH, KEY_TIME };
     
     public EarthQuakeDB (Context context){
 
-        this.helper= new EarthQuakeOpenHelper(context, EarthQuakeOpenHelper.DATABASE_NAME, null, EarthQuakeOpenHelper.DATABASE_VERSION);
-        this.db = helper.getWritableDatabase();
+        this.dbhelper= new EarthQuakeOpenHelper(context, EarthQuakeOpenHelper.DATABASE_NAME, null, EarthQuakeOpenHelper.DATABASE_VERSION);
+        this.db = dbhelper.getWritableDatabase();
     }
 
-    private static class EarthQuakeOpenHelper extends SQLiteOpenHelper {
+
+    public List <EarthQuake> getAll(){
+        return query(null, null);
+    }
+
+    public List <EarthQuake> getAllByMagnitude(int magnitude){
+        String where= KEY_MAGNITUDE + ">=?";
+
+        String [] whereArgs = {
+                String.valueOf(magnitude);
+        }
+        return query(where, whereArgs);
+
+    }
+
+    private List<EarthQuake> query() {
+
+        List <EarthQuake> earthQuakes = new ArrayList<>();
+
+            Cursor cursor;
+        cursor = db.query(
+                EarthQuakeOpenHelper.DATABASE_TABLE,
+                KEYS_ALL,
+                null,
+                null,
+                null,
+                null,
+                KEY_TIME + " DESC"
+        );
+
+        HashMap<String, Integer> indexes = new HashMap<>();
+        for (int i=0; i< KEYS_ALL.length; i++){
+            indexes.put(KEYS_ALL[i],cursor.getColumnIndex(KEYS_ALL[i]));
+        }
+        while (cursor.moveToNext()){
+
+            EarthQuake earthquake;
+            earthquake.set_id(cursor.getString(indexes.get(KEY_ID)));
+            earthquake.setPlace(cursor.getString(indexes.get(KEY_PLACE)));
+            earthquake.setMagnitude(cursor.getDouble(indexes.get(KEY_MAGNITUDE)));
+            earthquake.setLa(cursor.getDouble(indexes.get(KEY_LAT)));
+            earthquake.setLong(cursor.getDouble(indexes.get(KEY_LONG)));
+            earthquake.setUrl(cursor.getString(indexes.get(KEY_URL)));
+            earthquake.setTime(cursor.getString(indexes.get(KEY_TIME)));
+
+            earthQuakes.add(earthquake);
+
+
+
+        }
+
+
+
+        return earthQuakes;
+
+        }
+
+
+
+    public List<EarthQuake> query(String where, String  whereArgs) {
+
+        List <EarthQuake> earthQuakes = new ArrayList<>();
+
+        Cursor cursor;
+        cursor = db.query(
+                EarthQuakeOpenHelper.DATABASE_TABLE,
+                KEYS_ALL,
+                null,
+                where,
+                whereArgs,
+                null,
+                KEY_TIME + " DESC"
+        );
+
+        HashMap<String, Integer> indexes = new HashMap<>();
+        for (int i=0; i< KEYS_ALL.length; i++){
+            indexes.put(KEYS_ALL[i],cursor.getColumnIndex(KEYS_ALL[i]));
+        }
+        while (cursor.moveToNext()){
+
+            EarthQuake earthquake;
+            earthquake.set_id(cursor.getString(indexes.get(KEY_ID)));
+            earthquake.setPlace(cursor.getString(indexes.get(KEY_PLACE)));
+            earthquake.setMagnitude(cursor.getDouble(indexes.get(KEY_MAGNITUDE)));
+            earthquake.setLa(cursor.getDouble(indexes.get(KEY_LAT)));
+            earthquake.setLong(cursor.getDouble(indexes.get(KEY_LONG)));
+            earthquake.setUrl(cursor.getString(indexes.get(KEY_URL)));
+            earthquake.setTime(cursor.getString(indexes.get(KEY_TIME)));
+
+            earthQuakes.add(earthquake);
+
+
+
+        }
+
+
+
+        return earthQuakes;
+
+    }
+
+
+
+    public void createRow(EarthQuake earthquake) {
+
+        ContentValues newValues = new ContentValues();
+
+
+        newValues.put(Helper.KEY_ID, earthquake.get_id());
+        newValues.put(Helper.KEY_TIME, earthquake.getTime().getTime());
+        newValues.put(Helper.KEY_PLACE, earthquake.getPlace());
+        newValues.put(Helper.KEY_LONG, earthquake.getCoords().getLng());
+        newValues.put(Helper.KEY_LAT, earthquake.getCoords().getLat());
+        newValues.put(Helper.KEY_URL, earthquake.getUrl());
+        newValues.put(Helper.KEY_MAGNITUDE, earthquake.getMagnitude());
+        newValues.put(Helper.KEY_DEPTH, earthquake.getCoords().getDepth());
+
+
+        try{
+            db.insertOrThrow(EarthQuakeOpenHelper.DATABASE_TABLE, null, newValues);
+        }catch(SQLiteException ex) {
+
+        }
+
+    }
+
+
+
+    private class EarthQuakeOpenHelper extends SQLiteOpenHelper {
 
         private static final String DATABASE_NAME ="earthQuakes.db";
         private static final String DATABASE_TABLE = "EARTHQUAKES";
@@ -45,5 +198,11 @@ public class EarthQuakeDB {
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
         }
+
+
+
+
+
+
     }
 }
