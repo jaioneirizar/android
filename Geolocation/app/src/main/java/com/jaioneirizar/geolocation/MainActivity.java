123eae763fed5e4f075858ce2ca3e783efeabc04
jaioneirizar <jaioner14@gmail.com>
2015-04-07 14:39:17 +0200

    Geolocalización

--- Geolocation/app/src/main/java/com/jaioneirizar/geolocation/MainActivity.java
+++ Geolocation/app/src/main/java/com/jaioneirizar/geolocation/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends ActionBarActivity implements LocationListener 
 
     @Override
     public void onLocationChanged(Location location) {
-        
+
     }
 
     @Override
